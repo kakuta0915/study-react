b99730085dc3e79966b7ebf7a2132de8829ef81f
kakuta0915 <kakuta19960915@gmail.com>
2023-01-24 19:51:39 +0900

    ✨ linkコンポーネントを変更した

--- components/links.js
+++ components/links.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Links() {
   return (
-    <>
+    <div>
       <div>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -15,7 +15,7 @@ export default function Links() {
         >
           By{" "}
           <Image
-            src="/vercel.svg"
+            src="vercel.svg"
             alt="Vercel Logo"
             className={styles.vercelLogo}
             width={100}
@@ -28,14 +28,14 @@ export default function Links() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
         />
         <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+          <Image src="thirteen.svg" alt="13" width={40} height={31} priority />
         </div>
       </div>
 
@@ -97,6 +97,6 @@ export default function Links() {
           </p>
         </a>
       </div>
-    </>
+    </div>
   );
 }
